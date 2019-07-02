a44a601565c9282dde37a1156170360a8af887a2
Koshoo <benkosh@gmail.com>
2019-07-02 21:51:37 +0300

    add localstorage save feature

--- logic.js
+++ logic.js
@@ -1,8 +1,30 @@
 let myLibrary = [];
+let addedBooks = [];
+let index;
+getBooks();
 const bookTable = document.querySelector(".book-table");
 const newBookBtn = document.querySelector(".new-book");
 const form = document.querySelector("#form");
-let index = 0;
+
+addedBooks.forEach(function (book) {
+	createBookRow(book)
+	clearAdded();
+})
+
+function clearAdded() {
+	addedBooks = [];
+}
+
+function getBooks() {
+	addedBooks = JSON.parse(localStorage.getItem("book"));
+	if (addedBooks == null) {
+		addedBooks = [];
+	}
+	index = Number(localStorage.getItem("index"))
+	if(index == null){
+		index = 0;
+	}
+}
 
 function Book(author, title, pages, isRead) {
 	this.author = author;
@@ -13,12 +35,15 @@ function Book(author, title, pages, isRead) {
 	index++;
 }
 
-let lotr = new Book("J.R.R Tolkien", "Lord of The Rings", 490, "Yes");
-createBookRow(lotr);
+
 
 //creating a book row and adding to book to the library
 function createBookRow(book) {
 	myLibrary.push(book);
+	addedBooks.push(book);
+	localStorage.setItem("book", JSON.stringify(myLibrary));
+	localStorage.setItem("added", JSON.stringify(addedBooks));
+	localStorage.setItem("index", index);
 	tableRow = document.createElement("tr");
 	tableRow.className = index - 1;
 
@@ -37,7 +62,7 @@ function createBookRow(book) {
 		}
 	}
 	addDeleteBtn(tableRow);
-	addClearBtn(tableRow);
+	addReadBtn(tableRow);
 	appendRow(tableRow);
 }
 
@@ -55,7 +80,7 @@ function addDeleteBtn(tableRow) {
 
 }
 
-function addClearBtn(tableRow) {
+function addReadBtn(tableRow) {
 
 	let changeReadbtn = document.createElement("button")
 	changeReadbtn.textContent = "Change read status"
@@ -65,35 +90,52 @@ function addClearBtn(tableRow) {
 	tableRow.appendChild(changeReadbtn)
 }
 
-
-
 function addDeleteBtnListener(deleteBtn) {
 
 	deleteBtn.addEventListener("click", function (e) {
 		let bookPop = myLibrary.pop();
+
 		let currentEle = document.getElementsByClassName(e.path[0].attributes[0].textContent);
 		bookTable.removeChild(currentEle[0]);
-
 		for (let i = 0; i < myLibrary.length; i++) {
 			if (bookPop.index != e.path[0].attributes[0].textContent) {
 				myLibrary.push(bookPop);
 				bookPop = myLibrary.pop();
+
 			}
 		}
+
+		if (addedBooks.indexOf(bookPop) >= 0) {
+			addedBooks.splice(bookPop, 1);
+		}
+		localStorage.setItem("book", JSON.stringify(myLibrary));
+		localStorage.setItem("added", JSON.stringify(addedBooks));
 	});
 }
 
 function addReadBtnListener(changeReadbtn) {
 	changeReadbtn.addEventListener("click", function (e) {
-		console.log("k")
 		let readTextEle = document.querySelector(".read-" + e.path[0].attributes[0].value)
-		console.log(readTextEle)
-		if(readTextEle.textContent == "Yes"){
+
+		if (readTextEle.textContent == "Yes") {
 			readTextEle.textContent = "No"
-		}else{
+		} else {
 			readTextEle.textContent = "Yes"
 		}
-	})
+		
+		myLibrary.forEach(function(book) {
+			console.log(book.index)
+			if (book.index == e.path[0].attributes[0].value) {
+				if (book.isRead == "Yes") {
+					book.isRead = "No";
+				} else {
+					book.isRead = "Yes";
+				}
+			}
+		});
+	});
+	localStorage.setItem("book", JSON.stringify(myLibrary));
+	localStorage.setItem("added", JSON.stringify(addedBooks));
 }
 
 //Show form on button click
